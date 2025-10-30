c762115b75ca35ffc1f06965c9d7b9c8dfd506cb
Bilal Ahmed <bilallahmedd685@gmail.com>
2025-10-30 07:49:01 +0000

    create main.py

--- script/p.py
+++ script/p.py
@@ -3,11 +3,27 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.feature_extraction.text import TfidfVectorizer
-import os
 import streamlit as st
+import os
 
-# class EDA:
+
+class EDA:
     
-    # def eda(self):
+    def eda(self):
+        
+        file_path = "../Data/sentimentaldataset.csv"
+
+        
+        if os.path.exists(file_path):
+            
+            file=pd.read_csv(file_path)
+            print(file)
+        
+        else:
+            
+            print("invalid path...")
+
+obj=EDA()
+obj.eda()
         
         
