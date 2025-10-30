453c6432e5dd9a1801265371daa607c2d1759b4d
Bilal Ahmed <bilallahmedd685@gmail.com>
2025-10-30 07:12:29 +0000

    update code

--- script/p.py
+++ script/p.py
@@ -1,1 +1,13 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.feature_extraction.text import TfidfVectorizer
+import os
+import streamlit as st
 
+# class EDA:
+    
+    # def eda(self):
+        
+        
