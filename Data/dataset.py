9534500401f19cab911232098899d4e150c73a90
Bilal Ahmed <bilallahmedd685@gmail.com>
2025-11-25 09:45:21 +0000

    add another dataset

--- Data/dataset.py
+++ Data/dataset.py
@@ -1,17 +1,7 @@
-# import kagglehub
-
-# # Download latest version
-# path = kagglehub.dataset_download("abhi8923shriv/sentiment-analysis-dataset")
-
-# print("Path to dataset files:", path)
-
 import kagglehub
-import os
 
+# Download latest version
+# Choose Test.csv or Train.csv as per your requirement
 path = kagglehub.dataset_download("abhi8923shriv/sentiment-analysis-dataset")
-print("Downloaded path:", path)
 
-print("\nFiles inside the dataset folder:")
-for root, dirs, files in os.walk(path):
-    for file in files:
-        print(os.path.join(root, file))
+print("Path to dataset files:", path)
